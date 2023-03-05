21d447f9ba44196915dafd7e20d8054497c04439
richipargo <rtapia92@gmail.com>
2023-03-05 17:57:12 +0100

    support custom directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -l
 
+if [[ -z $DIRECTORY_PATH ]];
+then
+    echo "DIRECTORY_PATH env isn't set. Running in current directory"
+else
+    echo "Moving to specified directory path"
+    cd $DIRECTORY_PATH
+fi
+
 command="eb $1"
 
 $command
